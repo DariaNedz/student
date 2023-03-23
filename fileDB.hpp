f0560652eeca5a404bf4d79be38a119cc0b90473
Darkness <116870372+DariaNedz@users.noreply.github.com>
2023-03-23 09:31:31 +0100

    Create fileDB.hpp

---
+++ fileDB.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "person.hpp"
+
+using namespace std;
+
+class FileDB {
+
+	// Person FindPersonByNumber(int number);
+	Person FindPersonByNumber(int number, vector<Person>& people);
+	vector<Person> FindPersonByName(string name, vector<Person>& people);
+	vector<Person> FindPersonByAge(int age, vector<Person>& people);
+
+};
