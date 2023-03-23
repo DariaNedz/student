edabff0d04b164a01abbd3478c83a9fb2f8186a1
Darkness <116870372+DariaNedz@users.noreply.github.com>
2023-03-23 09:55:52 +0100

    Update fileDB.hpp

--- fileDB.hpp
+++ fileDB.hpp
@@ -6,8 +6,8 @@ using namespace std;
 class FileDB {
 
 	// Person FindPersonByNumber(int number);
-	Person FindPersonByNumber(int number, vector<Person>& people);
-	vector<Person> FindPersonByName(string name, vector<Person>& people);
-	vector<Person> FindPersonByAge(int age, vector<Person>& people);
+	Person FindPersonByNumber (int number, vector<Person>& people);
+	vector<Person> FindPersonByName (string name, vector<Person>& people);
+	vector<Person> FindPersonByAge (int age, vector<Person>& people);
 
 };
